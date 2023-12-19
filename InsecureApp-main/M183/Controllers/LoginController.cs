a296453bbec5f7b3381386f5caa99297a5ed7b12
Elena Tafelski <e.tafelski.inf21@stud.bbbaden.ch>
2023-12-19 17:39:40 +0100

    Logging

--- InsecureApp-main/M183/Controllers/LoginController.cs
+++ InsecureApp-main/M183/Controllers/LoginController.cs
@@ -3,8 +3,9 @@ using M183.Controllers.Helper;
 using M183.Data;
 using M183.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 
 namespace M183.Controllers
 {
@@ -12,11 +13,15 @@ namespace M183.Controllers
     [ApiController]
     public class LoginController : ControllerBase
     {
+        private readonly ILogger _logger;
         private readonly NewsAppContext _context;
+        private readonly IConfiguration _configuration;
 
-        public LoginController(NewsAppContext context)
+        public LoginController(ILogger<LoginController> logger, NewsAppContext context, IConfiguration configuration)
         {
+            _logger = logger;
             _context = context;
+            _configuration = configuration;
         }
 
         /// <summary>
@@ -35,17 +40,53 @@ namespace M183.Controllers
             {
                 return BadRequest();
             }
+            string username = request.Username;
+            string passwordHash = MD5Helper.ComputeMD5Hash(request.Password);
 
-            string sql = string.Format("SELECT * FROM Users WHERE username = '{0}' AND password = '{1}'", 
-                request.Username, 
-                MD5Helper.ComputeMD5Hash(request.Password));
+            User? user = _context.Users
+                .Where(u => u.Username == username)
+                .Where(u => u.Password == passwordHash)
+                .FirstOrDefault();
 
-            User? user= _context.Users.FromSqlRaw(sql).FirstOrDefault();
             if (user == null)
             {
+                _logger.LogWarning($"login failed for user '{request.Username}'");
                 return Unauthorized("login failed");
             }
-            return Ok(user);
+
+            _logger.LogInformation($"login successful for user '{request.Username}'");
+            return Ok(CreateToken(user));
+        }
+
+        private string CreateToken(User user)
+        {
+            string issuer = _configuration.GetSection("Jwt:Issuer").Value!;
+            string audience = _configuration.GetSection("Jwt:Audience").Value!;
+
+            List<Claim> claims = new List<Claim> {
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                    new Claim(JwtRegisteredClaimNames.NameId, user.Id.ToString()),
+                    new Claim(JwtRegisteredClaimNames.UniqueName, user.Username),
+                    new Claim(ClaimTypes.Role,  (user.IsAdmin ? "admin" : "user"))
+            };
+
+            string base64Key = _configuration.GetSection("Jwt:Key").Value!;
+            SymmetricSecurityKey securityKey = new SymmetricSecurityKey(Convert.FromBase64String(base64Key));
+
+            SigningCredentials credentials = new SigningCredentials(
+                    securityKey,
+                    SecurityAlgorithms.HmacSha512Signature);
+
+            JwtSecurityToken token = new JwtSecurityToken(
+                issuer: issuer,
+                audience: audience,
+                claims: claims,
+                notBefore: DateTime.Now,
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials: credentials
+             );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
         }
     }
 }
